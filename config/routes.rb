d8c65bbc3dc82049f6562437b2b80f7122863b94
takasan2024 <gyhuijk7896@yahoo.co.jp>
2024-04-21 15:10:08 +0900

    check4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"items#index"
-  resources :users, only: [:new ,:create] do
-    resources :buyers, only: :create
-end
-resources :users
 end
