475c0a92e989c8fd9dad6bd9bcf4252bb0360841
takasan2024 <gyhuijk7896@yahoo.co.jp>
2024-05-02 21:00:14 +0900

    ichiran-hyouji3

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   #has_one :address_information
   has_one_attached :image
-  has_one :buyer
+  #has_one :buyer
   
   validates :price, numericality: {only_integer: true, message: 'Half-width number'}, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}, presence: true
