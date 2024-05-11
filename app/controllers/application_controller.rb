2b4041f6b3723abc7add040bfdfcc4b6e24a7aa2
takasan2024 <gyhuijk7896@yahoo.co.jp>
2024-05-11 19:15:10 +0900

    Basic認証のIDとパスワード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
 
   private
   def configure_permitted_parameters
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+      username == 'takahiro' && password == '0987'
     end
   end
 end
